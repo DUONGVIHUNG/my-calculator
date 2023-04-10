e558dbc8e81c0ed7cc49be2b0c0cb251db95f169
Hung <duongvihung1995@gmail.com>
2023-04-11 00:40:37 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import Calc from './components/Calculator.vue'
+export default {
+  name: 'app',
+  components:{
+    Calc
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <Calc />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
